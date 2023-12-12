dafbc7c0d1930cca0cddffaa0f9c2a3cc8ba9387
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2023-12-12 09:25:54 +0530

    Initial Commit

---
+++ simple_database/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  simple_database
+//
+//  Created by Abhiram Rishi Prattipati on 12/12/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
