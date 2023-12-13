9cffee9a8c24733306c5b040959d0c77ceacff2e
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2023-12-13 10:55:08 +0530

    Done with step 2

---
+++ simple_database/Keywords.c
@@ -0,0 +1,8 @@
+//
+//  Keywords.c
+//  simple_database
+//
+//  Created by Abhiram Rishi Prattipati on 12/13/23.
+//
+
+#include "Keywords.h"
---
+++ simple_database/Keywords.h
@@ -0,0 +1,13 @@
+//
+//  Keywords.h
+//  simple_database
+//
+//  Created by Abhiram Rishi Prattipati on 12/13/23.
+//
+
+#ifndef Keywords_h
+#define Keywords_h
+
+#include <stdio.h>
+
+#endif /* Keywords_h */
