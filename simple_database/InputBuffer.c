1fcd0af1813df17552e643eccac484bb8aa947ee
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2023-12-13 09:06:44 +0530

    done with step 1

---
+++ simple_database/InputBuffer.c
@@ -0,0 +1,8 @@
+//
+//  InputBuffer.c
+//  simple_database
+//
+//  Created by Abhiram Rishi Prattipati on 12/13/23.
+//
+
+#include "InputBuffer.h"
---
+++ simple_database/InputBuffer.h
@@ -0,0 +1,13 @@
+//
+//  InputBuffer.h
+//  simple_database
+//
+//  Created by Abhiram Rishi Prattipati on 12/13/23.
+//
+
+#ifndef InputBuffer_h
+#define InputBuffer_h
+
+#include <stdio.h>
+
+#endif /* InputBuffer_h */
